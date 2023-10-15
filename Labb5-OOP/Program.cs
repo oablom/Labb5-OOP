e0a80a82fb2f3693a75abb677f3411fd32d9ecb5
oablom <oablom@gmail.com>
2023-10-15 22:31:18 +0200

    Add project files.

---
+++ Labb5-OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Circle cirkel1 = new Circle(5);
+Circle cirkel2 = new Circle(6);
+
+Console.WriteLine(cirkel1.GetCircleArea());
+Console.WriteLine(cirkel2.GetCircleArea());
+
+class Circle
+{
+    int _Radius;
+    float _pi = 3.141f;
+    public Circle(int radie)
+    {
+        _Radius = radie;
+
+
+    }
+    public float GetCircleArea()
+    {
+        return _Radius * _Radius * _pi;
+    }
+
+}
